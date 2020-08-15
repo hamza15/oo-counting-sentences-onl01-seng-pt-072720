7ee67025c53994bb6be65a1cfa3174b907d70fa5
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-15 21:52:54 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,40 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    if self == ""
+      return 0 
+    end
+    count = 1 
+    i = 0
+    final = self.chars
+    while i < final.length
+      if final[i] == "."
+        if final[i+1] == " "
+          count += 1
+        end 
+      elsif final[i] == "!"    
+        if final[i+1] == " "
+          count += 1
+        end 
+      elsif final[i] == "?"    
+        if final[i+1] == " "
+          count += 1
+        end
+      end
+      i += 1 
+    end
+    count
   end
 end
